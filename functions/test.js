25ff072134fa40c88984c9e28417546614bd850f
fajarot <fajarrahmadyanto@gmail.com>
2022-11-11 10:08:41 +0700

    add new test

--- functions/test.js
+++ functions/test.js
@@ -1,3 +1,13 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+fetch("https://api.nightbot.tv/1/commands", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
 const base =  (req, res) => {
     res.status(200).send(`Hello ${req.query.name}!`)
   }
